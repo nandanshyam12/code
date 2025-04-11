1eb36d365f2ec8b1757460c83cd8adf658b04f08
Nandan Shyam <111327751+nandanshyam12@users.noreply.github.com>
2025-04-11 06:19:30 +0530

    Delete pgm5.py

--- pgm5.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem import PorterStemmer,WordNetLemmatizer
-from nltk.corpus import wordnet
-stemmer=PorterStemmer()
-lemmatizer=WordNetLemmatizer()
-word="running"
-stemmed_word=stemmer.stem(word)
-lemmatized_word=lemmatizer.lemmatize(word,pos=wordnet.VERB)
-print("Stemmed Word:",stemmed_word)
-print("Lemmatized Word:",lemmatized_word)

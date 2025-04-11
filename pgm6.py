64cfaaac9716333bda56c959b06c7eeb298f336a
Nandan Shyam <111327751+nandanshyam12@users.noreply.github.com>
2025-04-11 06:19:36 +0530

    Delete pgm6.py

--- pgm6.py
+++
@@ -1,5 +0,0 @@
-from nltk.sentiment import SentimentIntensityAnalyzer
-sentiment_analyzer=SentimentIntensityAnalyzer()
-sentence="NLTK is a powerful library for natural language processing."
-sentiment_score=sentiment_analyzer.polarity_scores(sentence)
-print("Sentiment Score:",sentiment_score)

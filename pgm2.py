92a836379dcb2f4cef41669e2887300e37d4af4b
Nandan Shyam <111327751+nandanshyam12@users.noreply.github.com>
2025-04-11 06:19:24 +0530

    Delete pgm2.py

--- pgm2.py
+++
@@ -1,6 +0,0 @@
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-text="NLTK is a powerful library for NLP."
-tokens=word_tokenize(text)
-pos_tags=pos_tag(tokens)
-print("POS Tag:",pos_tags)
